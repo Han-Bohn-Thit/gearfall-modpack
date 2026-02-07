6314d659e673f72b891d714be8d86960185cb961
Ninad <kninad2008@gmail.com>
2026-02-07 21:23:12 +0530

    Seperate lava keys for each vent

--- overrides/kubejs/startup_scripts/new_stuff.js
+++ overrides/kubejs/startup_scripts/new_stuff.js
@@ -1,11 +1,24 @@
-//Lava Key
+//Lava Keys
 StartupEvents.registry('item', event => {
-  event.create('lava_key')
-  .displayName('Lava Key')
-  .maxStackSize(1)
-  .glow(true)
-  .tooltip("Right click on a vent to activate it!")
-  .rarity('epic')
+  const keys = [
+    ['asurine_lava_key', 'Asurine Lava Key', "Right click on an Active Asurine vent to activate it!"],
+    ['bauxite_lava_key', 'Bauxite Lava Key', "Right click on an Active Bauxite vent to activate it!"],
+    ['crimsite_lava_key', 'Crimsite Lava Key', "Right click on an Active Crimsite vent to activate it!"],
+    ['galena_lava_key', 'Galena Lava Key', "Right click on an Active Galena vent to activate it!"],
+    ['lignite_lava_key', 'Lignite Lava Key', "Right click on an Active Lignite vent to activate it!"],
+    ['ochrum_lava_key', 'Ochrum Lava Key', "Right click on an Active Ochrum vent to activate it!"],
+    ['veridium_lava_key', 'Veridium Lava Key', "Right click on an Active Veridium vent to activate it!"]
+  ];
+  for(let i=0; i<keys.length; i++)
+  {
+    event.create(keys[i][0])
+      .displayName(keys[i][1])
+      .maxStackSize(1)
+      .glow(true)
+      .tooltip(keys[i][2])
+      .rarity('epic')
+  }
+
 })
 
 //Impure Blocks
@@ -21,10 +34,10 @@ StartupEvents.registry('block', event => {
   ]);
   fruits.forEach((name, id) => {
     event.create(id)
-    .displayName(name)
-    .stoneSoundType()
-    .tagItem('gearfall:impure_blocks')
-    .opaque(true)
-    .fullBlock(true)
+      .displayName(name)
+      .stoneSoundType()
+      .tagItem('gearfall:impure_blocks')
+      .opaque(true)
+      .fullBlock(true)
   })
 })
