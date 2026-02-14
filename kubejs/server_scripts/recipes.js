3cbc3a74a61385fcfa63c12ffaa2075d0bf3258d
Ninad <kninad2008@gmail.com>
2026-02-14 12:20:32 +0530

    Replaced molten vents with mechanical extruder; Removed galena vent

---
+++ kubejs/server_scripts/recipes.js
@@ -0,0 +1,33 @@
+ServerEvents.recipes(event => {
+    //Remove all default extruding recipes
+    event.remove({ type: 'create_mechanical_extruder:extruding' })
+
+    const materials = [
+        ['asurine','create:asurine',"gearfall:biomes/cold"],
+        ['bauxite','tfmg:bauxite',"gearfall:biomes/mountain"],
+        ['crimsite','create:crimsite',"minecraft:is_nether"],
+        ['lignite','tfmg:lignite',"minecraft:is_overworld"],
+        ['ochrum','create:ochrum',"gearfall:biomes/hot"],
+        ['veridium','create:veridium',"gearfall:biomes/jungle"]
+    ]
+    materials.forEach(([name, block, biome]) => {
+        //Impure blocks extraction from unlocked vents
+        event.recipes.create_mechanical_extruder.extruding(
+            Item.of(`kubejs:impure_${name}`),
+            [BlockPredicate.of(block),BlockPredicate.of('minecraft:lava')]
+        )
+        .catalyst(BlockPredicate.of(`kubejs:unlocked_${name}_vent`))
+        .advanced(true)
+        .consumeBlocks([false,true])
+        .requirements([
+            RecipeRequirement.minSpeed(128.0),
+            RecipeRequirement.biomeTag(biome)
+        ]).id(`gearfall:recipes/extruding_${name}_vent`);
+        
+        //Item application for unlocking vents
+        event.recipes.create.item_application(
+            [`kubejs:unlocked_${name}_vent`],
+            [`kubejs:active_${name}_vent`, `kubejs:${name}_lava_key`]
+        ).id(`gearfall:recipes/unlocking_${name}_vent_manual_only`)
+    });
+})
--- kubejs/startup_scripts/new_stuff.js
+++ kubejs/startup_scripts/new_stuff.js
@@ -1,5 +1,5 @@
-//Materials to generate Lava Keys and Impure Blocks for
-const materials = ['Asurine','Bauxite','Crimsite','Lignite','Ochrum','Veridium','Galena'];
+//Materials to generate Lava Keys and Vent Blocks for
+const materials = ['Asurine','Bauxite','Crimsite','Lignite','Ochrum','Veridium'];
 
 //Lava Keys
 StartupEvents.registry('item', event => {  
@@ -15,9 +15,27 @@ StartupEvents.registry('item', event => {
   });
 })
 
-//Impure Blocks
+//Vent Blocks
 StartupEvents.registry('block', event => {
   materials.forEach(material => {
+    event.create(`dormant_${material.toLowerCase()}_vent`)
+      .displayName(`Dormant ${material} Vent`)
+      .stoneSoundType()
+      .opaque(true)
+      .fullBlock(true)
+      .tagItem('gearfall:dormant_vents');
+    event.create(`active_${material.toLowerCase()}_vent`)
+      .displayName(`Active ${material} Vent`)
+      .stoneSoundType()
+      .opaque(true)
+      .fullBlock(true)
+      .tagItem('gearfall:active_vents');
+    event.create(`unlocked_${material.toLowerCase()}_vent`)
+      .displayName(`Unlocked ${material} Vent`)
+      .stoneSoundType()
+      .opaque(true)
+      .fullBlock(true)
+      .tagItem('gearfall:unlocked_vents');
     event.create(`impure_${material.toLowerCase()}`)
       .displayName(`Impure ${material}`)
       .stoneSoundType()
